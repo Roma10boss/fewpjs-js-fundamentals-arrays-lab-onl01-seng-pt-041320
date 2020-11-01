b3cb81d04f7ba9de42ecc1910cca7b0155912e50
Romario Gustave <romagustave10@gmail.com>
2020-11-01 15:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"]; 
 append.push('odie'); 
+append;
 
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.unshift("odie")
+prepend.unshift("odie");
+prepend;
 
 const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.pop();
