4a2dab10e5e6c4fe14ab663691425012a39cd8fe
Romario Gustave <romagustave10@gmail.com>
2020-11-01 15:56:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const prepend = ["Milo", "Otis", "Garfield"];
 prepend.unshift("odie")
 
 const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop 
 
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
